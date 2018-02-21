4b8e06d17b253dda00e91f58f4600c94fd2cf16f
nihotamor <36220585+nihotamor@users.noreply.github.com>
2018-02-21 19:30:24 +0900

    Create ItemTestFood.java

---
+++ src/main/java/doph/niho/cookmod/food/ItemTestFood.java
@@ -0,0 +1,10 @@
+package doph.niho.cookmod.food;
+
+import doph.niho.cookmod.init.BaseFood;
+
+public class ItemTestFood extends BaseFood{
+	public ItemTestFood() {
+		super("testfood", 2, 4, false);
+		this.setUnlocalizedName("TestFood");
+	}
+}
