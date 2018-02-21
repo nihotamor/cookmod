919a8d2795222a1ef19ff3bedbc91638bffead63
nihotamor <36220585+nihotamor@users.noreply.github.com>
2018-02-21 20:04:10 +0900

    Create Define.java

---
+++ src/main/java/doph/niho/cookmod/system/Define.java
@@ -0,0 +1,8 @@
+package doph.niho.cookmod.system;
+
+public class Define {
+	public static final String MODID = "cookmod";
+	public static final String MOD_NAME = "Cook Mod";
+	public static final String MOD_VERSION = "0.00";
+	public static final String MOD_MC_VERSION = "[1.12,1.12.2]";
+}
